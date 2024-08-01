29eb015c58a7960c5c946217c63ff45d077d0c8a
Juan perri <111884460+perrijuan@users.noreply.github.com>
2024-08-01 01:49:16 -0300

    Update configuration2.nix

--- configuration2.nix
+++ configuration2.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-
+#configuração para computadores com inteis e placas graficas nvidia 
 #configuração pessoal para um setup para desenvolvimento e uso de jogos 
 #uso de jogos usando a camada de compatibilidade wine :)
 
